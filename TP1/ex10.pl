a90046a90668dd8f25d6f975f4d100053715b76f
joaoDossena <joaopdossena@gmail.com>
2020-09-29 12:59:44 +0100

    Adding more rules to ex10.pl

--- TP1/ex10.pl
+++ TP1/ex10.pl
@@ -1,3 +1,5 @@
+:- use_module(library(between)).
+
 comprou(joao, honda).
 ano(honda, 1997).
 
@@ -9,6 +11,7 @@ valor(uno, 7000).
 
 
 pode_vender(PESSOA, CARRO, ANO_ATUAL) :-
+	between(1900, 2100, ANO_ATUAL),
 	comprou(PESSOA, CARRO),
 	ano(CARRO, ANO_COMPRA),
 	(ANO_ATUAL - ANO_COMPRA =< 10),
