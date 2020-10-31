51d871e4fd4d17aa1302d9fc6ebc7a73ca2ff4b7
joaoDossena <joaopdossena@gmail.com>
2020-10-31 16:43:48 +0000

    Fixing bug and documenting predicates

--- proj/src/display.pl
+++ proj/src/display.pl
@@ -1,34 +1,40 @@
-
+% Initial configuration of board
 initialBoard([
 	[[empty], [empty], [empty], [empty], [black, black, black, black, black, black]],
 	[[empty], [empty], [empty], [empty], [empty]],
 	[[empty], [empty], [empty], [empty], [empty]],
 	[[empty], [empty], [empty], [empty], [empty]],
-	[[white, white, white, white, white, white], [empty], [empty], [empty], [empty]],
+	[[white, white, white, white, white, white], [empty], [empty], [empty], [empty]]
 	]).
 
+% Initial configuration of cubes out of the board
+initialWhiteCubes([whiteCube, whiteCube, whiteCube, whiteCube, whiteCube, whiteCube, whiteCube, whiteCube, whiteCube]).
+initialBlackCubes([blackCube, blackCube, blackCube, blackCube, blackCube, blackCube, blackCube, blackCube, blackCube]).
 
+% Symbols and what they represent
 symbol(empty, '.').
 symbol(black, 'B').
 symbol(blackCube, 'C').
 symbol(white, 'W').
 symbol(whiteCube, 'K').
 
+% Row letters
 letter(1, 'A').
 letter(2, 'B').
 letter(3, 'C').
 letter(4, 'D').
 letter(5, 'E').
 
-
+% Prints game board X
 printBoard(X) :-
     nl,
     write('   | 1 | 2 | 3 | 4 | 5 | \n'),
     write('---|---|---|---|---|---|\n'),
     printMatrix(X, 1).
 
-printMatrix([], 5). %Talvez mudemos pra 5
 
+% Prints matrix
+printMatrix([], 5).
 printMatrix([Head|Tail], N) :-
     letter(N, L),
     write(' '),
@@ -39,8 +45,8 @@ printMatrix([Head|Tail], N) :-
     write('\n---|---|---|---|---|---|\n'),
     printMatrix(Tail, N1).
 
+% Prints a line
 printLine([]).
-
 printLine([Head|Tail]) :-
     symbol(Head, S),
     write(S),
