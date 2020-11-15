63b70add6ecae9cfaf1f3f4498eea052b5a96d1f
joaoDossena <joaopdossena@gmail.com>
2020-11-15 21:08:01 +0000

    Adding predicates that handle input

---
+++ proj/src/input.pl
@@ -0,0 +1,45 @@
+
+inputRow(NewRow) :-
+    readRow(Row),
+    validateRow(Row, NewRow).
+
+inputColumn(NewColumn) :-
+    readColumn(Column),
+    validateColumn(Column).
+
+
+readRow(Row) :-
+    write('  > Row    '),
+    read(Row).
+
+readColumn(Column) :-
+    write('  > Column '),
+    read(Column).
+
+
+% Checks if row is valid
+validateRow('A', NewRow) :-
+    NewRow = 1.
+validateRow('B', NewRow) :-
+    NewRow = 2.
+validateRow('C', NewRow) :-
+    NewRow = 3.
+validateRow('D', NewRow) :-
+    NewRow = 4.
+validateRow('E', NewRow) :-
+    NewRow = 5.
+validateRow(_Row, NewRow) :-
+    write('ERROR: That row is not valid!\n\n'),
+    readRow(Input),
+    validateRow(Input, NewRow).
+
+% Checks if column is valid
+validateColumn(1).
+validateColumn(2).
+validateColumn(3).
+validateColumn(4).
+validateColumn(5).
+validateColumn(_Column) :-
+    write('ERROR: That column is not valid!\n\n'),
+    readColumn(Input),
+    validateColumn(Input).
