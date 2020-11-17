2c432b96be994051089a5e2268064beeabcad02e
joaoDossena <joaopdossena@gmail.com>
2020-11-17 20:47:59 +0000

    Asks for coordinates successfully

--- proj/src/logic.pl
+++ proj/src/logic.pl
@@ -1,15 +1,20 @@
 
-whitePlayerTurn(OldState, NewState, 'Person') :-
+whitePlayerTurn([OldBoard, OldWhites, OldBlacks], _NewState, 'Person') :-
   nl,nl,nl,write('\n------------------ PLAYER 1 (WHITE) -------------------\n\n'),
-  display_game(OldState, white),
+  display_game([OldBoard, OldWhites, OldBlacks], white),
   nl,write('What stack do you want to move?'),nl,
-  getCurrentCoords(OldState, white).
+  getCurrentCoords(OldBoard, white, Row, Column),
+  nl,write('Where to?'),nl,
+  getFutureCoords(NewRow, NewColumn).
 
-blackPlayerTurn(OldState, NewState, 'Person') :-
+blackPlayerTurn([OldBoard, OldWhites, OldBlacks], _NewState, 'Person') :-
   nl,nl,nl,write('\n------------------ PLAYER 2 (BLACK)  -------------------\n\n'),
   display_game(OldState, black),
   write('What stack do you want to move?'),nl,
-  getCurrentCoords(OldState, black).
+  getCurrentCoords(OldBoard, black, Row, Column),
+  nl,write('Where to?'),nl,
+  getFutureCoords(NewRow, NewColumn).
+
       
 
 /*
@@ -19,13 +24,14 @@ blackPlayerTurn(Board, NewBoard, 'Computer') :-
   write('\n----------------- COMPUTER (BLACK) ------------------\n\n').
 */
 
-getCurrentCoords(GameState, Player) :-
-      inputRow(NewRow),
-      inputColumn(NewColumn),
-      %% nl,
-      ColumnIndex is NewColumn - 1,
-      RowIndex is NewRow - 1,
-      checkStack(GameState, Player, RowIndex, ColumnIndex).
+getFutureCoords(RowIndex, ColumnIndex) :-
+  inputRow(RowIndex),
+  inputColumn(ColumnIndex).
+
+getCurrentCoords(Board, Player, RowIndex, ColumnIndex) :-
+      inputRow(RowIndex),
+      inputColumn(ColumnIndex),
+      checkStack(Board, Player, RowIndex, ColumnIndex).
 
   
 %Loop do jogo, em que recebe a jogada de cada jogador e verifica o estado do jogo a seguir.
@@ -41,25 +47,18 @@ gameLoop(OldState, Player1, Player2) :-
    )
   ).
 
-searchInRow(Stack, 0, Content) :-
-  Content = Stack.
+getElementFromBoard(Board, RowIndex, ColumnIndex, Elem) :-
+  A is RowIndex,
+  B is ColumnIndex,
+  nth1(A, Board, Row),
+  nth1(B, Row, Elem).
 
-searchInRow([Column|RestOfColumns], ColumnIndex, Content) :-
-  NewColumnIndex is ColumnIndex - 1,
-  searchInRow(RestOfColumns, NewColumnIndex, Content).
 
-searchInMatrix([Row|RestOfRows], 0, ColumnIndex, Content) :-
-  searchInRow(Row, ColumnIndex, Content).
-
-searchInMatrix([Row|RestOfRows], RowIndex, ColumnIndex, Content) :-
-  NewRowIndex is RowIndex - 1,
-  searchInMatrix(RestOfRows, NewRowIndex, ColumnIndex, Content).
-
-checkStack([Board, _WhiteCubesLeft, _BlackCubesLeft], white, RowIndex, ColumnIndex) :-
-  searchInMatrix(Board, RowIndex, ColumnIndex, Stack),
+checkStack(Board, white, RowIndex, ColumnIndex) :-
+  getElementFromBoard(Board, RowIndex, ColumnIndex, Stack),
   whiteStack(Stack).
-checkStack([Board, _WhiteCubesLeft, _BlackCubesLeft], black, RowIndex, ColumnIndex) :-
-  searchInMatrix(Board, RowIndex, ColumnIndex, Stack),
+checkStack(Board, black, RowIndex, ColumnIndex) :-
+  getElementFromBoard(Board, RowIndex, ColumnIndex, Stack),
   blackStack(Stack).
 
 
