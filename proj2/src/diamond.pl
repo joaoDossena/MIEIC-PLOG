7fabab61b88318e0b7bcf28d0161051a86a6a043
joaoDossena <joaopdossena@gmail.com>
2020-12-28 19:17:24 +0000

    Draws board and defines size of solution board

--- proj2/src/diamond.pl
+++ proj2/src/diamond.pl
@@ -1,13 +1,26 @@
+:- consult('utils.pl').
+
 :- use_module(library(clpfd)).
 
 
+% DiamondIndexList is a list with the index number of each diamond on the original problem board
+
+% Examples to copy & paste:
+% solve([1, 4, 7, 18, 33, 42, 43, 47, 49], 7, 7, SolutionBoard).
+
 % Main function that solves puzzle
-solve() :-
+% solve(+DiamondIndexList, +NumberOfRows, +NumberOfColumns, -SolutionBoard)
+solve(DiamondIndexList, NumberOfRows, NumberOfColumns, SolutionBoard) :-
+	% Draws Problem Board
+	draw(1, NumberOfColumns, NumberOfRows, DiamondIndexList),
+
 	% Timer starts
 	statistics(walltime, [Start,_]),
 
 	% Decision Variables
-
+	length(DiamondIndexList, NumberOfDiamonds),
+	length(SolutionBoard, NumberOfRows),
+	build_cols(SolutionBoard, NumberOfColumns),
 
 	% Restrictions
 
