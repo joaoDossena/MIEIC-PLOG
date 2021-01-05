cefc3bfb3bfd7957802886be2c305bbbe7cc1eab
joaoDossena <joaopdossena@gmail.com>
2021-01-05 20:22:08 +0000

    IDK

--- proj2/src/diamond.pl
+++ proj2/src/diamond.pl
@@ -59,111 +59,6 @@ noMore([H|T], ElemNotToRepeat, ElemIndex, CurrIndex) :-
 	noMore(T, ElemNotToRepeat, ElemIndex, NewCurrentIndex).
 
 
-%% check_number_of_equals([], _ULIndex, _CurrentIndex, Total, Total).
-%% check_number_of_equals([_H], _ULIndex, _CurrentIndex, Total, Total).
-%% check_number_of_equals([_H, NH|T], ULIndex, CurrentIndex, Number, Total):-
-%% 	CurrentIndex < ULIndex,
-%% 	NewCurrentIndex is CurrentIndex + 1,
-%% 	check_number_of_equals([NH|T], ULIndex, NewCurrentIndex, Number, Total). 
-%% check_number_of_equals([H, NH|_T], ULIndex, CurrentIndex, Total, Total):-
-%% 	CurrentIndex >= ULIndex,
-%% 	H #\= NH.
-%% check_number_of_equals([H, NH|T], ULIndex, CurrentIndex, Number, Total):-
-%% 	CurrentIndex >= ULIndex,
-%% 	H #= NH,
-%% 	NewNumber is Number + 1,
-%% 	NewCurrentIndex is CurrentIndex + 1,
-%% 	check_number_of_equals([NH|T], ULIndex, NewCurrentIndex, NewNumber, Total). 
-
-
-%% check_columns(_List, _NColumns, _Index, _Element, Count, Number, 0):- %false
-%% 	Count > Number.
-%% check_columns(List, _NColumns, Index, Element, Number, Number, 1):- %true
-%% 	element(Index, List, TestElement),
-%% 	TestElement #\= Element.
-%% check_columns(List, NColumns, Index, Element, Count, Number, IsSquare):-
-%% 	element(Index, List, TestElement),
-%% 	TestElement #= Element,
-%% 	NewCount is Count + 1,
-%% 	NewIndex is Index + NColumns,
-%% 	Number < NColumns,
-%% 	check_columns(List, NColumns, NewIndex, Element, NewCount, Number, IsSquare).
-%% check_columns(List, NColumns, Index, Element, _Count, Number, 0):- %false
-%% 	element(Index, List, TestElement),
-%% 	TestElement #= Element,
-%% 	%% NewCount is Count + 1,
-%% 	%% NewIndex is Index + NColumns,
-%% 	Number >= NColumns.
-
-
-%% check_square(_List, NRows, NColumns, Index, _IsSquare):-
-%% 	Index =:= (NRows * NColumns).
-%% check_square(List, _NRows, NColumns, Index, IsSquare):-
-%% 	check_number_of_equals(List, Index, 1, 0, Number),
-%% 	write('Number: '),
-%% 	write(Number), nl,
-%% 	element(Index, List, ElementToCheck),
-%% 	check_columns(List, NColumns, Index, ElementToCheck, 0, Number, IsSquare).
-
-
-% check_cell(_List, _NRows, _NColumns, FinalIndex, FinalIndex). 
-% 	%% Index > (NRows * NColumns).
-
-% check_cell(List, NRows, NColumns, Index, FinalIndex) :-
-% %% 	%% Index =< (NRows * NColumns),
-% 	domain([IsUpperLeftCorner], 0, 1),
-% 	Index mod NColumns =\= 0,
-% 	Temp is Index // NRows + 1,
-% 	Temp < NRows,
-% 	check_upper_left_corner(List, NRows, NColumns, Index, IsUpperLeftCorner),
-% 	%% IsUpperLeftCorner =:= 1,
-% 	element(Index, List, Elem),
-% 	RightIndex is Index + 1,
-% 	element(RightIndex, List, RightElem),
-% 	BottomIndex is Index + NColumns,
-% 	element(BottomIndex, List, BottomElem),
-% 	Elem #= RightElem,
-% 	Elem #= BottomElem,
-% %% 	write('Index: '),
-% %% 	write(Index), nl,
-% %% 	% write('IsUpperLeftCorner: '),
-% %% 	% write(IsUpperLeftCorner), nl,
-% %% 	% check_square %%TODO
-% %% 	% check_square(List, NRows, NColumns, Index, IsSquare),
-% %% 	% write('IsSquare: '),
-% %% 	% write(IsSquare), nl,
-% %% 	% IsUL #=> IsSquare
-% %% 	% IsUpperLeftCorner #=> IsSquare,
-% 	NextIndex is Index + 1,
-% 	check_cell(List, NRows, NColumns, NextIndex, FinalIndex).
-
-
-% check_cell(List, NRows, NColumns, Index, FinalIndex) :- %% Not upperleft
-% %% 	%% Index < (NRows * NColumns),
-% 	domain([IsUpperLeftCorner], 0, 1),
-% 	check_upper_left_corner(List, NRows, NColumns, Index, IsUpperLeftCorner),
-% 	%% IsUpperLeftCorner =:= 0,
-% 	write('Index: '),
-% 	write(Index), nl,
-% 	NextIndex is Index + 1,
-% 	check_cell(List, NRows, NColumns, NextIndex, FinalIndex).
-
-% check_cell(List, NRows, NColumns, Index, FinalIndex) :- %%  UperLeft and 1x1 square
-% 	domain([IsUpperLeftCorner], 0, 1),
-% 	check_upper_left_corner(List, NRows, NColumns, Index, IsUpperLeftCorner),
-% 	%% IsUpperLeftCorner =:= 1,
-% 	write('Index of 1x1 square: '),
-% 	write(Index), nl,
-% 	element(Index, List, Elem),
-% 	%% write('before noMore: '),nl,
-% 	noMore(List, Elem, Index, 1),
-% 	%% write('after noMore: '),nl,
-% 	NextIndex is Index + 1,
-% 	check_cell(List, NRows, NColumns, NextIndex, FinalIndex).
-
-
-
-
 
 % count_numbers_in_row(List, NRow, NCol, OldElem, Total, Total):-
 % 	nth1(NRow, List, Row),
@@ -172,16 +67,12 @@ noMore([H|T], ElemNotToRepeat, ElemIndex, CurrIndex) :-
 
 count_numbers_in_row(_List, TotalColumns, _NRow, NewNCol, _Elem, 0, _NotFinished):-
 	NewNCol > TotalColumns.
-	
-
 count_numbers_in_row(List, TotalColumns, NRow, NCol, OldElem, Count, NotFinished):-
 	NCol =< TotalColumns,
 	nth1(NRow, List, Row),
 	nth1(NCol, Row, Elem),
 	Count #= CountNext + NotFinished,
-
 	NotFinishedNext #<=> NotFinished #/\ (Elem #= OldElem),
-	
 	NewNCol is NCol + 1,
 	count_numbers_in_row(List, TotalColumns, NRow, NewNCol, Elem, CountNext, NotFinishedNext).
 
@@ -189,8 +80,6 @@ count_numbers_in_row(List, TotalColumns, NRow, NCol, OldElem, Count, NotFinished
 
 count_equal_rows(_List, TotalRows, _TotalColumns, NRow, _NCol, _Elem, 0, _Sum, _NotFinished):-
 	NRow > TotalRows.
-
-
 count_equal_rows(List, TotalRows, TotalColumns, NRow, NCol, Elem, Count, Sum, NotFinished):-
 	NRow =< TotalRows,
 	count_numbers_in_row(List, TotalColumns, NRow, NCol, Elem, RowCount, 1),
@@ -215,45 +104,30 @@ check_square(List, Index, NRows, NColumns, IsSquare):-
 
 	nth1(NRow, List, Row),
 	element(NCol, Row, Elem),
-
+	%% trace,
 	count_numbers_in_row(List, NColumns, NRow, NCol, Elem, Sum, 1),
-
-	% trace,
+	%% write('Caling new function'),
 	count_equal_rows(List, NRows, NColumns, NRow, NCol, Elem, Count, Sum, 1),
-	% notrace,
+	%% write('Ended function'),
+	%% notrace,
 
 	(Count #= Sum) #<=> IsSquare.
 
 
-%% check_square(List, Index, NRows, NColumns, IsSquare):-
-%% 	TCol is Index mod NColumns,
-%% 	NRow is Index // NRows + 1,
-%% 	TCol == 0,
-%% 	NCol = NColumns,
-
-%% 	nth1(NRow, List, Row),
-%% 	element(NCol, Row, Elem),
-
-%% 	count_numbers_in_row(List, NColumns, NRow, NCol, Elem, Sum, 1),
-
-%% 	count_equal_rows(List, NRows, NColumns, NRow, NCol, Elem, Count, Sum, 1),
-
-%% 	(Count #= Sum) #<=> IsSquare.
 
 
 
 iterateBoard(_FlatList, _List, _NRows, _NColumns, Index, Index, UpperLeftCount, UpperLeftCount).
 iterateBoard(FlatList, List, NRows, NColumns, Index, FinalIndex, UpperLeftCount, TotalCount):-
+	Index < FinalIndex,
 	%% write('Final Index: '), write(FinalIndex), nl,
 	write('Index: '), write(Index), nl,
 	check_upper_left_corner(FlatList, NRows, NColumns, Index, IsUpperLeftCorner),
 	%% write('Is UL? '), write(IsUpperLeftCorner),nl,
-	check_square(List, Index, NRows, NColumns, IsSquare),
 	IsUpperLeftCorner #=> IsSquare,
 	UpperLeftCountNext #= UpperLeftCount + IsUpperLeftCorner,
-	% IsSquare #<=> 1,
+	check_square(List, Index, NRows, NColumns, IsSquare),
 	NewIndex is Index + 1,
-	%% write('New Index: '), write(NewIndex), nl,
 
 	iterateBoard(FlatList, List, NRows, NColumns, NewIndex, FinalIndex, UpperLeftCountNext, TotalCount).
 
@@ -282,14 +156,14 @@ check_upper_left_corner(List, _NRows, NColumns, Index, IsUpperLeftCorner) :- %tr
 	element(Index, List, CurrElement),
 	%% LeftIndex is Index - 1,
 	%% element(LeftIndex, List, LeftElement),
-	RightIndex is Index + 1,
-	element(RightIndex, List, RightElem),
-	BottomIndex is Index + NColumns,
-	element(BottomIndex, List, BottomElem),
+	%% RightIndex is Index + 1,
+	%% element(RightIndex, List, RightElem),
+	%% BottomIndex is Index + NColumns,
+	%% element(BottomIndex, List, BottomElem),
 	check_appearance(List, Index, CurrElement, 1, NeverAppeared),
 	%% (CurrElement #\= LeftElement) #/\
 
-	(NeverAppeared #/\ (CurrElement #= RightElem) #<=> (CurrElement #= BottomElem))  #=> IsUpperLeftCorner. % If they have different values, then the current element is an Upper Left Corner
+	NeverAppeared #=> IsUpperLeftCorner. % If they have different values, then the current element is an Upper Left Corner
 
 % In case the element is not on the first row but is in the first column
 check_upper_left_corner(List, _NRows, NColumns, Index, IsUpperLeftCorner) :- %true
@@ -331,14 +205,15 @@ check_upper_left_corner(List, _NRows, NColumns, Index, IsUpperLeftCorner) :- %tr
 	%% element(TopIndex, List, TopElement),
 	%% LeftIndex is Index - 1,
 	%% element(LeftIndex, List, LeftElement),
-	RightIndex is Index + 1,
-	element(RightIndex, List, RightElem),
-	BottomIndex is Index + NColumns,
-	element(BottomIndex, List, BottomElem),
+	%% RightIndex is Index + 1,
+	%% element(RightIndex, List, RightElem),
+	%% BottomIndex is Index + NColumns,
+	%% element(BottomIndex, List, BottomElem),
 	
 	check_appearance(List, Index, CurrElement, 1, NeverAppeared),
 	%% (((CurrElement #\= TopElement) #/\ (CurrElement #\= LeftElement)) #/\ 
-	(((CurrElement #= RightElem) #<=> (CurrElement #= BottomElem)) #/\ NeverAppeared)  #=> IsUpperLeftCorner. % If it is different from both the top and the left, then the current element is an Upper Left Corner
+	%% ( #/\ 
+	NeverAppeared  #=> IsUpperLeftCorner. % If it is different from both the top and the left, then the current element is an Upper Left Corner
 
 
 % In case the element is in the last Row
@@ -362,12 +237,12 @@ check_upper_left_corner(List, _NRows, NColumns, Index, IsUpperLeftCorner) :- %tr
 	%% element(TopIndex, List, TopElement),
 	%% LeftIndex is Index - 1,
 	%% element(LeftIndex, List, LeftElement),
-	RightIndex is Index + 1,
-	element(RightIndex, List, RightElem),
+	%% RightIndex is Index + 1,
+	%% element(RightIndex, List, RightElem),
 	
 	check_appearance(List, Index, CurrElement, 1, NeverAppeared),
 	%% ((CurrElement #\= TopElement) #/\ (CurrElement #\= LeftElement) #/\ 
-	(NeverAppeared #/\ (CurrElement #\= RightElem)) #=> IsUpperLeftCorner. % If it is different from both the top and the left, then the current element is an Upper Left Corner
+	(NeverAppeared ) #=> IsUpperLeftCorner . % If it is different from both the top and the left, then the current element is an Upper Left Corner
 
 % In case the element is in the last Row and Last column
 check_upper_left_corner(List, _NRows, NColumns, Index, IsUpperLeftCorner) :- %true
@@ -388,23 +263,17 @@ check_upper_left_corner(List, _NRows, NColumns, Index, IsUpperLeftCorner) :- %tr
 	%% ((CurrElement #\= TopElement) #/\ (CurrElement #\= LeftElement)) 
 	NeverAppeared #=> IsUpperLeftCorner. % If it is different from both the top and the left, then the current element is an Upper Left Corner
 
-
 setDiamondOrder([]).
 setDiamondOrder([_]).
 setDiamondOrder([H, H1|T]):-
 	H1 #> H,
 	setDiamondOrder([H1|T]).
 
-%% SE NÃO ME ENGANO, ANALISEI TODOS OS CASOS VERDADEIROS
-% In the other cases it is false
-%% check_upper_left_corner(_List, _NRows, _NColumns, _Index, IsUpperLeftCorner) :-
-%% 	IsUpperLeftCorner #=> 0. %false
-
 % DiamondIndexList is a list with the index number of each diamond on the original problem board
 
 % Examples to copy & paste:
 % solve([1, 4, 7, 18, 33, 42, 43, 47, 49], 7, 7, S).
-% solve([1, 7], 2, 4, S).
+% solve([1, 3], 2, 4, S).
 
 % Main function that solves puzzle
 % solve(+DiamondIndexList, +NumberOfRows, +NumberOfColumns, -SolutionBoard).
@@ -413,7 +282,7 @@ solve(DiamondIndexList, NumberOfRows, NumberOfColumns, Vars) :-
 	draw(1, NumberOfColumns, NumberOfRows, DiamondIndexList),
 
 	% Timer starts
-	statistics(walltime, [Start,_]),
+	%% statistics(walltime, [Start,_]),
 
 	% Decision Variables
 	length(DiamondIndexList, NumberOfDiamonds), % Gets number of diamonds in problem
@@ -437,7 +306,7 @@ solve(DiamondIndexList, NumberOfRows, NumberOfColumns, Vars) :-
 	% trace,
 	iterateBoard(Vars, SolutionBoard, NumberOfRows, NumberOfColumns, 1, FinalIndex, 0, ULCount),
 
-	NumberOfDiamonds #= ULCount,
+	NumberOfDiamonds = ULCount,
 	% notrace,
 	%% write('After checking cells'), nl,
 	/*V1,V1,T
@@ -456,14 +325,14 @@ solve(DiamondIndexList, NumberOfRows, NumberOfColumns, Vars) :-
 	labeling([], Vars),
 	% notrace,
 	%% findall(Count, (between(1, NumberOfDiamonds, N), count(Vars, N, Count)), CountList),
-	%% write(Rest), nl,
+	write('Number of UpperLeft: '), write(ULCount), nl,
 	%% write(CountList), nl,
 	%% write(Vars), nl,
 
 	% Timer ends
-	statistics(walltime, [End,_]),
-	Time is End - Start,
-    format('Duration: ~3d s~n', [Time]),
+	%% statistics(walltime, [End,_]),
+	%% Time is End - Start,
+	%% format('Duration: ~3d s~n', [Time]),
 
 	
 	% % Draws Problem Board
